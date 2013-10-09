d90c7974dd5f5caa3cdbb2294e6ea4fe503893eb
sgryt <christensen.mikkel@gmail.com>
2013-10-09 20:46:24 +0200

    Added role assignment file name property

--- Automation/ConvertToClaimRulesCommand.cs
+++ Automation/ConvertToClaimRulesCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using System.Text;
@@ -10,5 +12,34 @@ namespace Automation
     [Cmdlet(VerbsData.ConvertTo, "ClaimRules")]
     public class ConvertToClaimRulesCommand
     {
+        private string roleAssignmentFile;
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = false)]
+        public string RoleAssignmentFile
+        { 
+            get 
+            {
+                return this.roleAssignmentFile;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+
+                if (!File.Exists(value))
+                {
+                    throw new ArgumentException(
+                        string.Format(
+                            CultureInfo.CurrentCulture,
+                            "The specified filename '{0}' could not be found on disk",
+                            value)
+                        );
+                }
+
+                this.roleAssignmentFile = value;
+            }
+        }
     }
 }
