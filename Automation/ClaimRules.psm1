638af9a799fb48561a12753615fa62c03c5585d0
sgryt <christensen.mikkel@gmail.com>
2013-10-11 15:02:57 +0200

    Fixed syntax on claim rule to make AD FS UI happy

--- Automation/ClaimRules.psm1
+++ Automation/ClaimRules.psm1
@@ -6,9 +6,10 @@ param (
     Process 
 	{ 
 @"
+@RuleTemplate = "MapClaims"
 @RuleName = "{0}"
-c:[Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role", Value == "{1}"]
- => issue(Type = "{2}", Value = "{3}");
-"@ -f $RolePermission.Title, $RolePermission.Role, $RolePermission.Permission.AbsoluteUri, $RolePermission.Value
+c:[Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role", Value =~ "^(?i){1}$"]
+ => issue(Type = "{2}", Issuer = c.Issuer, OriginalIssuer = c.OriginalIssuer, Value = "{3}", ValueType = c.ValueType);
+"@ -f $RolePermission.Title, $RolePermission.Role.Replace(" ", "\ "), $RolePermission.Permission.AbsoluteUri, $RolePermission.Value
 	} 
 }
